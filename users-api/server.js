ac1f8d59c44eef9d0c11073cac86b68b459ad550
Emilio <emiliogarb@hotmail.com>
2023-10-06 18:45:10 -0600

    First Commit

---
+++ users-api/server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+/*List Users*/
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+/*Add User*/
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+/*Detail User*/
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
+})
+/*Delete User*/
+app.delete('/deleteUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      delete data["user" + 2];
+       
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+/*Launch Server*/
+var server = app.listen(8081, function () {
+   console.log("Users API listening at http://localhost:8081");
+})
